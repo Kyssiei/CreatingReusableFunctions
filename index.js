db4576817084327b6193e155414aee98e07559da
Kyssiei <Kyssiei.kay@gmail.com>
2025-01-27 18:28:58 -0500

    part 2

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-//** PART 1 */
+//** ======================= PART 1: Thinking Functionally ==================*/
+console.log("==================== PART 1: Thinking Functionally ==================");
+
 //Todo: Take an array of numbers and return the sum 
 let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
@@ -44,18 +46,96 @@ console.log(longestString(string));
 
 //Todo: Take an array of strings, and a number and return an array of the strings that are longer than the given number 
 
-// strLongThan (['mango', 'cherry', 'watermelon', 'blueberry', 'strawberry', 3]);
+let array = ['say', 'hello', 'in', 'the', 'morning']
+let n
 
-// let n = 3
+function strLongThan(arr, n){
+    let result =[ ];
+    for (let str of arr) {
+        if (str.length > n) {
+            result.push(str);
+        }
+    }
+    return result
+}
 
-// function strLongThan(arr, n){
-//    let result = [];  
-//    for(let str of arr){
-//     if(str.length > n){
-//         result.push(str)
-//     }
-//    }
-//    return result; 
-// }
+console.log(strLongThan(['say', 'hello', 'in', 'the', 'morning'], 3));
 
-// console.log(strLongThan(['mango', 'cherry', 'watermelon', 'blueberry', 'strawberry']));
+
+//Todo: Take a number, n, and print every number between 1 and n without using loops. Use recursion.
+
+const printNumbers = n =>{
+    if (n < 1) return;
+    printNumbers(n - 1);
+    console.log(n);
+}
+console.log("Print n");
+printNumbers(10)
+
+
+//**========================= PART 2: Thinking Methodically ============ */
+console.log("==================== PART 2: Thinking Methodically ===================");
+
+const people = [
+    { id: "42", name: "Bruce", occupation: "Knight", age: "41" },
+    { id: "48", name: "Barry", occupation: "Runner", age: "25" },
+    { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
+    { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
+    { id: "7", name: "Bilbo", occupation: "None", age: "111" }
+];
+
+//Todo - Sort array by age
+const sortedByAge = [...people].sort((a, b) => Number(a.age) - Number(b.age));
+console.log(sortedByAge);
+
+
+//todo - Filter array (remove > 50)
+const filteredByAge = people.filter(person => Number(person.age) <= 50);
+console.log(filteredByAge);
+
+
+//todo - Map array (change & increase)
+const mappedPeople = people.map(person => ({
+    ...person,
+    job: person.occupation,
+    age: String(Number(person.age) + 1)
+}));
+
+console.log(mappedPeople);
+
+
+//todo - Use reduce method for sum (then result for average)
+const sumOfAges = people.reduce((sum, person) => sum + Number(person.age), 0);
+const averageAge = sumOfAges / people.length;
+  
+console.log(sumOfAges); // Sum of ages
+console.log(Math.ceil(averageAge)); // Average age
+  
+//** ============================== PART 3: Thinking Critically ============================= */
+console.log("====================== PART 3: Thinking Critically =====================");
+
+//Todo: Take an object and increment its age field.
+function incAge(obj) {
+    if (!obj.age) obj.age = 0;
+    obj.age += 1;
+    obj.updated_at = new Date();
+}
+  
+//Todo: Take an object, make a copy, and increment the age field of the copy. Return the copy.
+function incAgeCopy(obj) {
+    const newObj = { ...obj };
+    if (!newObj.age) newObj.age = 0;
+    newObj.age += 1;
+    newObj.updated_at = new Date();
+    return newObj;
+}
+  
+//log data 
+const person = { name: "Superman", age: 35, updated_at: new Date("2024-12-31") };
+incAge(person);
+console.log("Increment by reference:", person);
+
+const newPerson = incAgeCopy(person);
+newPerson.updated_at.setTime(new Date("2025-01-01").getTime())
+console.log("Original after modifying:", newPerson);
+console.log("Copied:", person);
